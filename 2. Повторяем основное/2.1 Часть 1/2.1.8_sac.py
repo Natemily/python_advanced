f8adae7afcb996ea356130c559768252c029290e
Natemily <nerezko.n@gmail.com>
2024-06-21 09:56:46 +0300

    Исправление

--- 2. Повторяем основное/2.1 Часть 1/2.1.8_sac.py
+++ 2. Повторяем основное/2.1 Часть 1/2.1.8_sac.py
@@ -1,4 +1,21 @@
 n = input()
-a = ""
-for i in range(len(n)):
-    1
+k = len(n)-1
+m = []
+if len(n) % 3 == 1:
+    m.append(n[0])
+    k = 1
+elif len(n) % 3 == 2:
+    m.append(n[0 : 2])
+    k = 2
+elif len(n) <= 3:
+    m.append(n)
+elif len(n) % 3 == 0:
+    m.append(n[0 : 3])
+    k = 3
+    
+if len(n) >= 4:
+    while k < len(n):
+        m.append("," + n[k:k+3])
+        k += 3
+        
+print(*m, sep="")
