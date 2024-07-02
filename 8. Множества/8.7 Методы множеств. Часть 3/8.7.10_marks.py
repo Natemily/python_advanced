be417f4585018cd25a510dceed784116499f5ddc
Natemily <nerezko.n@gmail.com>
2024-07-02 17:04:08 +0300

    Урок информатики

--- 8. Множества/8.7 Методы множеств. Часть 3/8.7.10_marks.py
+++ 8. Множества/8.7 Методы множеств. Часть 3/8.7.10_marks.py
@@ -1,4 +1,5 @@
-st1, st2, st3 = set(input()), set(input()), set(input())
+a, b, c = input().split(), input().split(), input().split()
+st1, st2, st3 = {int(i) for i in a}, {int(i) for i in b}, {int(i) for i in c}
 st12 = st1.intersection(st2)
 st123 = st12.difference(st3)
 print(*sorted(st123, reverse=True))
